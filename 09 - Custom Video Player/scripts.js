fabf0ef63e5cca83b0774cbe05176564105f7cfe
josue Avuh <jkomi2003@gmail.com>
2024-01-19 11:56:29 +0100

    finish custom video player

--- 09 - Custom Video Player/scripts.js
+++ 09 - Custom Video Player/scripts.js
@@ -3,6 +3,9 @@ const toggle=document.querySelector('.toggle')
 const play=document.querySelector('.toggle')
 const progress=document.querySelector('.progress')
 const progressBar=document.querySelector('.progress__filled')
+const ranges=document.querySelectorAll('.player__slider')
+const skipButtons=document.querySelectorAll('.player__button')
+
 
 function toggledPlay(){
     const method=video.paused ? 'play' : 'pause'
@@ -24,20 +27,36 @@ function scrub(e) {
     video.currentTime = scrubTime;
   }
 
+function skip(){
+    video.currentTime+=parseFloat(this.dataset.skip)
+    console.log(this.dataset)
+
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
+
 video.addEventListener('click',toggledPlay)
 video.addEventListener('play', updateButton)
 video.addEventListener('pause', updateButton)
 video.addEventListener('timeupdate', handleProgress)
 
-function handleProgress(){
-    const percent = (video.currentTime/ video.duration)*100;
-    progressBar.style.flexBasis=`${percent}%`;
-}
-
 toggle.addEventListener('click', toggledPlay)
+skipButtons.forEach(range=> range.addEventListener('click',skip))
+ranges.forEach(range=> range.addEventListener('change',handleRangeUpdate))
+ranges.forEach(range=> range.addEventListener('mousemove',handleRangeUpdate))
+
 
 let mousedown = false;
 progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
 progress.addEventListener('mousedown', () => mousedown = true);
 progress.addEventListener('mouseup', () => mousedown = false);
+
+
+// KEY TAKEAWAYS
+
+// 1-OBTAIN THE ELEMENTS USING THE DOM 
+// 2-WRITE THE FUNCTIONS TO HAVE AN EFFECT ON EACH OBTAINED ELEMENTS AS UPU MIGHT DESIRE IT
+// 3-ADD THE EVENTLISTENERS TO MAP THE CORRESPONDING ELEMENTS TO FUNCTIONS THROUGH EVENTS
