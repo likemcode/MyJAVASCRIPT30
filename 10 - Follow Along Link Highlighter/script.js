c66dae556b4c2dd16acd2d97cc4a5bbf3bc47020
josue Avuh <jkomi2003@gmail.com>
2024-01-23 11:49:05 +0100

    comments in FLA JS

--- 10 - Follow Along Link Highlighter/script.js
+++ 10 - Follow Along Link Highlighter/script.js
@@ -1,22 +1,27 @@
-const triggers = document.querySelectorAll('a');
-const highlight = document.createElement('span');
-highlight.classList.add('highlight');
-document.body.appendChild(highlight);
+// Select the Elements (links in this case)
+const links=document.querySelectorAll('a')
+// crete span element to add for highligth and give a class to it 
+const highlight=document.createElement('span') 
+highlight.className='highlight'
+// Add the class to the body of the document
+document.body.appendChild(highlight)
 
+// function to highlight the link
 function highlightLink() {
-  const linkCoords = this.getBoundingClientRect();
-  console.log(linkCoords);
+  const linkCoords=this.getBoundingClientRect()
+  //take coords of linkcoords
   const coords = {
     width: linkCoords.width,
     height: linkCoords.height,
     top: linkCoords.top + window.scrollY,
     left: linkCoords.left + window.scrollX
   };
-
-  highlight.style.width = `${coords.width}px`;
-  highlight.style.height = `${coords.height}px`;
-  highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+  // Add the styles to the highlight element
+  highlight.style.height=`${coords.height}px`
+  highlight.style.width=`${coords.width}px`
+  highlight.style.transform=`translate(${coords.left}px, ${coords.top}px)`
+  ;
 
 }
-
-triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
+// addEventListener to each link for the function to operate
+links.forEach(a=>a.addEventListener('mouseenter',highlightLink))
