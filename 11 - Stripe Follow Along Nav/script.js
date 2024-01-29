1f781a1828d51ecf99341b5e1c1a5ba82667bbfa
josue Avuh <jkomi2003@gmail.com>
2024-01-29 11:34:53 +0100

    speech Detection

--- 11 - Stripe Follow Along Nav/script.js
+++ 11 - Stripe Follow Along Nav/script.js
@@ -1,6 +1,6 @@
 const triggers = document.querySelectorAll('.cool > li');
-  const background  = document.querySelector('.dropdownBackground');
-  const nav  = document.querySelector('.top');
+const background  = document.querySelector('.dropdownBackground');
+const nav  = document.querySelector('.top');
 
   function handleEnter() {
     this.classList.add('trigger-enter');
